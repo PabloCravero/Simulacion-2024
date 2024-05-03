0f9f8e3a8557abb292b195d62a8030076bb6e5e3
Luca Gini <luca.giniclc@gmail.com>
2024-05-03 16:14:49 -0300

    Corrección if y parámetros de entrada. Chequear comentario en capital infinito de martingala

--- TP_1.2_Estudio_economico_ruleta/TP_1.2_Ruleta.py
+++ TP_1.2_Estudio_economico_ruleta/TP_1.2_Ruleta.py
@@ -10,143 +10,148 @@ estrategia = str(sys.argv[4])
 capital = str(sys.argv[5])
 
 if (numero >= 0) and (numero <= 36):
-    if len(sys.argv) != 3 or tiradas != "-n":
+    if len(sys.argv) == 6:
         print("Uso: python programa.py -n <num_valores>")
-        # sys.exit(1)
+        for _ in range(corridas):
+            valores = [random.randint(0, 37) for _ in range(tiradas)]
+            print("Valores generados:", valores)
+            if capital == 'f':
+                monto = float(input("Ingrese el monto total a jugar: "))
+            else:
+                monto = float('inf')
+            apuesta = float(input("Ingrese el monto de la apuesta inicial: "))
+            apuesta_inicial = apuesta
+            monto_inicial = monto
+            bancarrota = 0
+            fibonacci = [0, apuesta_inicial]
+            ganancias = 0 #???
+            for n in valores:
+                if estrategia == 'm':  #martingala
+                    if n != numero:
+                        if capital == 'f':
+                            monto -= apuesta
+                            apuesta = apuesta * 2
+                            print("Monto actual: ", monto)
+                            if monto <= 0:
+                                bancarrota += 1
+                                print("Bancarrota")
+                                monto = monto_inicial
+                                apuesta = apuesta_inicial
+                                print('monto bancarrota: ', monto)
+                                print('monto inicial: ', monto_inicial)
+                        else:
+                            ganancias = ganancias - apuesta
+                            apuesta = apuesta * 2
+                            print('Ganancias: ', ganancias)
+                            #rta = (str(input("Desea seguir jugando? (s/n): "))).lower()
+                            #if rta == 'n':
+                            #    break'''
+                    else:
+                        if capital == 'f':
+                            monto += apuesta
+                            apuesta = apuesta_inicial
+                            print("Monto actual: ", monto)
+                        else:
+                            ganancias = ganancias + apuesta
+                            apuesta = apuesta_inicial
+                            print('Ganancias: ', ganancias)
+                            #rta = (str(input("Desea seguir jugando? (s/n): "))).lower()
+                            #if rta == 'n':
+                            #    break
+                elif estrategia == 'd': #d'alembert
+                    if n != numero:
+                        if capital == 'f':
+                            monto -= apuesta
+                            apuesta = apuesta + 1
+                            print("Monto actual: ", monto)
+                            if monto <= 0:
+                                bancarrota += 1
+                                print("Bancarrota")
+                                monto = monto_inicial
+                                apuesta = apuesta_inicial
+                                print('monto bancarrota: ', monto)
+                                print('monto inicial: ', monto_inicial)
+                        else:
+                            ganancias = ganancias - apuesta
+                            apuesta = apuesta + 1
+                            print('Ganancias: ', ganancias)
+                    else:
+                        if capital == 'f':
+                            monto += apuesta
+                            apuesta = apuesta - 1
+                            print("Monto actual: ", monto)
+                        else:
+                            ganancias = ganancias + apuesta
+                            apuesta = apuesta - 1
+                            print('Ganancias: ', ganancias)
+                elif estrategia == 'f': # Fibonacci
+                    if n != numero:
+                        if capital == 'f':
+                            monto -= apuesta
+                            apuesta = fibonacci[-1] + fibonacci[-2] 
+                            fibonacci.append(apuesta)  
+                            print("fibonacci: ", fibonacci)
+                            print("Monto actual: ", monto)
+                            if monto <= 0:
+                                bancarrota += 1
+                                print("Bancarrota")
+                                monto = monto_inicial
+                                apuesta = apuesta_inicial
+                                fibonacci = [0, apuesta_inicial]
+                                print('monto bancarrota: ', monto)
+                                print('monto inicial: ', monto_inicial)
+                        else: 
+                            ganancias = ganancias - apuesta
+                            apuesta = fibonacci[-1] + fibonacci[-2] 
+                            fibonacci.append(apuesta)
+                            print('apuesta: ', apuesta)
+                            print('Ganancias: ', ganancias)
+                            print(fibonacci)
+                    else:
+                        if capital == 'f':
+                            monto += apuesta
+                            if len(fibonacci) > 3:
+                                apuesta = fibonacci[-3] 
+                            fibonacci.append(apuesta)
+                            print("Monto actual: ", monto)
+                            print("fibonacci: ", fibonacci)
+                        else:
+                            ganancias = ganancias + apuesta
+                            if len(fibonacci) > 3:
+                                apuesta = fibonacci[-3]
+                            fibonacci.append(apuesta)
+                            print('apuesta: ', apuesta)
+                            print('Ganancias: ', ganancias)
+                            print(fibonacci)
+                elif estrategia == 'p': #paroli o martingala inversa
+                    if n == numero:
+                        if capital == 'f':
+                            monto += apuesta
+                            apuesta = apuesta * 2
+                            print("Monto actual: ", monto)
+                        else:
+                            ganancias = ganancias + apuesta
+                            apuesta = apuesta * 2
+                            print('Ganancias: ', ganancias)
+                    else:
+                        if capital == 'f':
+                            monto -= apuesta
+                            apuesta = apuesta_inicial
+                            print("Monto actual: ", monto)
+                            if monto <= 0:
+                                bancarrota += 1
+                                print("Bancarrota")
+                                monto = monto_inicial
+                                apuesta = apuesta_inicial
+                                print('monto bancarrota: ', monto)
+                                print('monto inicial: ', monto_inicial)
+                        else: 
+                            ganancias = ganancias - apuesta
+                            apuesta = apuesta_inicial
+                            print('Ganancias: ', ganancias)
     else:
         print('Mal ingresados los datos')
-
-    for _ in range(corridas):
-        valores = [random.randint(0, 1) for _ in range(tiradas)]
-        print("Valores generados:", valores)
-        if capital == 'f':
-            monto = float(input("Ingrese el monto total a jugar: "))
-        else:
-            monto = float('inf')
-        apuesta = float(input("Ingrese el monto de la apuesta inicial: "))
-        apuesta_inicial = apuesta
-        monto_inicial = monto
-        bancarrota = 0
-        fibonacci = [0, apuesta_inicial]
-        ganancias = 0 #???
-        for n in valores:
-            if estrategia == 'm':  #martingala
-                if n != numero:
-                    if capital == 'f':
-                        monto -= apuesta
-                        apuesta = apuesta * 2
-                        print("Monto actual: ", monto)
-                        if monto <= 0:
-                            bancarrota += 1
-                            print("Bancarrota")
-                            monto = monto_inicial
-                            apuesta = apuesta_inicial
-                            print('monto bancarrota: ', monto)
-                            print('monto inicial: ', monto_inicial)
-                    else:
-                        ganancias = ganancias - apuesta
-                        apuesta = apuesta * 2
-                        print('Ganancias: ', ganancias)
-                else:
-                    if capital == 'f':
-                        monto += apuesta
-                        apuesta = apuesta_inicial
-                        print("Monto actual: ", monto)
-                    else:
-                        ganancias = ganancias + apuesta
-                        apuesta = apuesta_inicial
-                        print('Ganancias: ', ganancias)
-            elif estrategia == 'd': #d'alembert
-                if n != numero:
-                    if capital == 'f':
-                        monto -= apuesta
-                        apuesta = apuesta + 1
-                        print("Monto actual: ", monto)
-                        if monto <= 0:
-                            bancarrota += 1
-                            print("Bancarrota")
-                            monto = monto_inicial
-                            apuesta = apuesta_inicial
-                            print('monto bancarrota: ', monto)
-                            print('monto inicial: ', monto_inicial)
-                    else:
-                        ganancias = ganancias - apuesta
-                        apuesta = apuesta + 1
-                        print('Ganancias: ', ganancias)
-                else:
-                    if capital == 'f':
-                        monto += apuesta
-                        apuesta = apuesta - 1
-                        print("Monto actual: ", monto)
-                    else:
-                        ganancias = ganancias + apuesta
-                        apuesta = apuesta - 1
-                        print('Ganancias: ', ganancias)
-            elif estrategia == 'f': # Fibonacci
-                if n != numero:
-                    if capital == 'f':
-                        monto -= apuesta
-                        apuesta = fibonacci[-1] + fibonacci[-2] 
-                        fibonacci.append(apuesta)  
-                        print("fibonacci: ", fibonacci)
-                        print("Monto actual: ", monto)
-                        if monto <= 0:
-                            bancarrota += 1
-                            print("Bancarrota")
-                            monto = monto_inicial
-                            apuesta = apuesta_inicial
-                            fibonacci = [0, apuesta_inicial]
-                            print('monto bancarrota: ', monto)
-                            print('monto inicial: ', monto_inicial)
-                    else: 
-                        ganancias = ganancias - apuesta
-                        apuesta = fibonacci[-1] + fibonacci[-2] 
-                        fibonacci.append(apuesta)
-                        print('apuesta: ', apuesta)
-                        print('Ganancias: ', ganancias)
-                        print(fibonacci)
-                else:
-                    if capital == 'f':
-                        monto += apuesta
-                        if len(fibonacci) > 3:
-                            apuesta = fibonacci[-3] 
-                        fibonacci.append(apuesta)
-                        print("Monto actual: ", monto)
-                        print("fibonacci: ", fibonacci)
-                    else:
-                        ganancias = ganancias + apuesta
-                        if len(fibonacci) > 3:
-                            apuesta = fibonacci[-3]
-                        fibonacci.append(apuesta)
-                        print('apuesta: ', apuesta)
-                        print('Ganancias: ', ganancias)
-                        print(fibonacci)
-            elif estrategia == 'p': #paroli o martingala inversa
-                if n == numero:
-                    if capital == 'f':
-                        monto += apuesta
-                        apuesta = apuesta * 2
-                        print("Monto actual: ", monto)
-                    else:
-                        ganancias = ganancias + apuesta
-                        apuesta = apuesta * 2
-                        print('Ganancias: ', ganancias)
-                else:
-                    if capital == 'f':
-                        monto -= apuesta
-                        apuesta = apuesta_inicial
-                        print("Monto actual: ", monto)
-                        if monto <= 0:
-                            bancarrota += 1
-                            print("Bancarrota")
-                            monto = monto_inicial
-                            apuesta = apuesta_inicial
-                            print('monto bancarrota: ', monto)
-                            print('monto inicial: ', monto_inicial)
-                    else: 
-                        ganancias = ganancias - apuesta
-                        apuesta = apuesta_inicial
-                        print('Ganancias: ', ganancias)
+        sys.exit(1)
             
                     
                     
